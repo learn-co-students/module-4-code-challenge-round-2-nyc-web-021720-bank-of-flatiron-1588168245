a8f27702c38117eaa2552cc775926455c27227f2
komald4 <komald.4@gmail.com>
2020-04-29 10:41:27 -0400

    2nd commit

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -3,7 +3,31 @@ import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
+ let URL = 'http://localhost:6001/transactions'
+
 class AccountContainer extends Component {
+  constructor() {
+    super()
+    this.state = {
+      dataInfo: [],
+      transactions: [],
+      searchForm: '' //clears the field 
+    }
+  }
+
+  //render dataInfo using fetch. Should use DidMount
+
+    componentDidMount() {
+      fetch(URL)
+      console.log(URL)
+      .then(response => response.json())
+      .then(results => this.setState({
+        dataInfo: results,
+        transactions: results
+      }))
+    }
+
+
   render() {
     return (
       <div>
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,14 +1,15 @@
 import React from "react";
 
-const Transaction = () => {
-  return (
+const Transaction = ({date, description, category, amount}) => {
+   return (
     <tr>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
+      <td>{date}</td>
+      <td>{description}</td>
+      <td>{category}</td>
+      <td>{amount}</td>
     </tr>
-  );
-};
+   ) 
+}
+ 
 
-export default Transaction;
+export default Transaction
